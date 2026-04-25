49f9b9d8e0dc46d6b9a748af53da2e1189408873
mjallerp-dev <mjallerp@unicartagena.edu.co>
2026-04-24 22:42:54 -0500

    feat(Nit): Crear value object Nit con constructor y validación

---
+++ src/domain/empresa/Nit.java
@@ -0,0 +1,15 @@
+package domain.empresa;
+
+public class Nit {
+    private final String nit;
+
+    public Nit (String nit){
+        if (nit==null || nit.trim().isEmpty()){
+            throw new IllegalArgumentException("El NIT no puede ser nulo o vacío");
+        }
+        else if (!nit.matches("\\d{5,13}")){ //Esto asegura que el nit contenga solo dígitos y tenga entre 5 y 13 caracteres.
+            throw new IllegalArgumentException("El NIT debe contener solo dígitos, entre 5 y 13 caracteres");
+        }
+        this.nit = nit;
+    }
+}
